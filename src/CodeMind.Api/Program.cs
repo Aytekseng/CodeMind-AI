cf9ac186fcb1ce3b7c5140a6e039955a4433c436
Aytek <aytek_aksu09@hotmail.com>
2026-08-09 00:07:33 +0300

    feat: configure db context and generate initial migrations

--- src/CodeMind.Api/Program.cs
+++ src/CodeMind.Api/Program.cs
@@ -1,6 +1,11 @@
+using CodeMind.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
